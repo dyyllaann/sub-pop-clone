3a55fdbbcd8c8e56379c577f3567630da58e6133
dyyllaann <dylan.arveson@gmail.com>
2022-09-06 19:38:11 -0700

    Fix redirect from ../catalog to ..catalog/items

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 // GET home page.
 router.get('/', function(req, res) {
-  res.redirect('/catalog');
+  res.redirect('/catalog/items');
+});
+
+// GET home page.
+router.get('/catalog', function(req, res) {
+  res.redirect('/catalog/items');
 });
 
 module.exports = router;
